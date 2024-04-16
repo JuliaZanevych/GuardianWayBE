0b862cc0e8a577a757a3663cf4201f7b8a581d18
Julia Zanevych <Julia.Zanevych@gmail.com>
2024-04-16 20:22:18 +0300

    - create base project structure - add connection with postgres - add JWT authorization

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager, jwt_required
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+from app import auth_blueprint, blacklist
+from resources.db import db
 
 
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    app = Flask(__name__)
+
+    app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Change this!
+    app.config['SQLALCHEMY_DATABASE_URI'] = '******************'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_BLACKLIST_ENABLED'] = True
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+    db.init_app(app)
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blacklist(jwt_header, jwt_payload):
+        jti = jwt_payload["jti"]
+        return jti in blacklist
+
+    app.register_blueprint(auth_blueprint)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+
+
+    @app.route('/test', methods=['get'])
+    @jwt_required()
+    def test():
+        return '123'
+
+
+    app.run(debug=True)
---
+++ resources/db/models/user.py
@@ -0,0 +1,15 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from resources.db.db import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
